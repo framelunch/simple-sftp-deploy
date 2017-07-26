242af44a955785ec6f39a88db24c5832593d06a7
kaave <abe@framelunch.jp>
2017-07-26 18:33:24 +0900

    Add key auth setting

--- deploy.js
+++ deploy.js
@@ -5,14 +5,14 @@ const { Client } = require('ssh2');
 
 require('dotenv').config();
 
-const { DEPLOY_HOST, DEPLOY_PORT, DEPLOY_USER, DEPLOY_PASS, DEPLOY_TARGET, DEPLOY_DIR_FROM_ROOT } = process.env;
+const { DEPLOY_HOST, DEPLOY_PORT, DEPLOY_USER, DEPLOY_PASS, DEPLOY_TARGET, DEPLOY_DIR_FROM_ROOT, DEPLOY_KEY_PATH, DEPLOY_KEY_PASSPHRASE } = process.env;
 
 if (!DEPLOY_HOST) {
   throw new Error('Invalid params: DEPLOY_HOST');
 } else if (!DEPLOY_USER) {
   throw new Error('Invalid params: DEPLOY_USER');
-} else if (!DEPLOY_PASS) {
-  throw new Error('Invalid params: DEPLOY_PASS');
+} else if (!DEPLOY_PASS && !DEPLOY_KEY_PATH) {
+  throw new Error('Invalid params: DEPLOY_PASS and DEPLOY_KEY_PATH');
 } else if (!DEPLOY_TARGET) {
   throw new Error('Invalid params: DEPLOY_TARGET');
 } else if (!DEPLOY_DIR_FROM_ROOT) {
@@ -26,9 +26,19 @@ if (!DEPLOY_HOST) {
 const connectOption = {
   host: DEPLOY_HOST,
   port: parseInt(DEPLOY_PORT || '22', 10),
-  username: DEPLOY_USER,
-  password: DEPLOY_PASS
+  username: DEPLOY_USER
 };
+if (DEPLOY_KEY_PATH) {
+  connectOption.privateKey = fs.readFileSync(DEPLOY_KEY_PATH);
+
+  if (DEPLOY_KEY_PASSPHRASE) {
+    connectOption.passphrase = DEPLOY_KEY_PASSPHRASE;
+  }
+}
+
+if (DEPLOY_PASS) {
+  connectOption.password = DEPLOY_PASS;
+}
 // upload remote path
 const uploadRemoteDirectory = DEPLOY_TARGET;
 // upload local directory
